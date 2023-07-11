999b890bf0f4febe2ac6ea64bb09824821fbd2d5
papito1111 <patrykrychly@gmail.com>
2023-07-11 23:07:17 +0200

    construktor na jsonach

---
+++ src/MySpot.api/Controllers/ReservationsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+[Route(template:"reservations")]
+public class ReservationController : Controller
+{
+	[HttpGet(template:"get")]
+	public void Get()
+	{
+
+	}
+
+    [HttpPost]
+    public void Post([FromBody]Reservation reservation)
+    {
+
+    }
+}
+
+
+	
---
+++ src/MySpot.api/Models/reservation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Reservation
+{
+	public string EmployeeNAme { get; set; }
+	public string ParkingSpotNAme { get; set; }
+	public string LicensePlate { get; set; }
+	public DateTime Date { get; set; }
+	
+}
