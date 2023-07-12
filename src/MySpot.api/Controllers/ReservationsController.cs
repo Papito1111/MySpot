ba2a4c3fc560f211b84fd0f246afd2f0b1212b6a
papito1111 <patrykrychly@gmail.com>
2023-07-12 20:31:35 +0200

    rezerwacja, metody

--- src/MySpot.api/Controllers/ReservationsController.cs
+++ src/MySpot.api/Controllers/ReservationsController.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 
+[ApiController]
+
 [Route(template:"reservations")]
 public class ReservationController : Controller
 {
-	[HttpGet(template:"get")]
+    private int _id = 1;
+    private readonly List<Reservation> reservations = new();
+    private readonly List<string> _parkingSpotNames = new()
+    {
+        "P1", "P2","P3", "P4"
+    };
+
+	[HttpGet]
 	public void Get()
 	{
-
-	}
+        
+    }
 
     [HttpPost]
-    public void Post([FromBody]Reservation reservation)
+    public void Post(Reservation reservation)
     {
+        if (_parkingSpotNames.All(x => x != reservation.ParkingSpotNAme))
+        {
+            HttpContext.Response.StatusCode = 400;
+            return;
+        }
 
+        reservation.Id = _id;
+        reservation.Date = DateTime.UtcNow.AddDays(1).Date;
+        _id++;
+        
     }
 }
 
--- src/MySpot.api/Models/reservation.cs
+++ src/MySpot.api/Models/reservation.cs
@@ -2,6 +2,7 @@
 
 public class Reservation
 {
+	public int Id { get; set; }
 	public string EmployeeNAme { get; set; }
 	public string ParkingSpotNAme { get; set; }
 	public string LicensePlate { get; set; }
